60a66594ae3eb7932e109503e78eea4baca9dc4f
zeynallisahile <tu7dfbyuu@code.edu.az>
2022-04-08 11:59:36 +0400

    Task Completed

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Student> students = new List<Student>();
+
+            Student student1 = new Student()
+            {
+                FullName = " Adil Bayramov",
+                No = 1                            
+            };
+
+            Student student2 = new Student()
+            {
+                FullName = " Rasim Eliyev",
+                No = 2
+            };
+
+            Student student3 = new Student()
+            {
+                FullName = " Nihad Qurbanov",
+                No = 3
+            };
+            do
+            {    
+                
+            menu:
+
+            Console.WriteLine("1. Tələbə əlavə et");
+            Console.WriteLine("2. Tələbəyə imtahan əlavə et");
+            Console.WriteLine("3. Tələbənin bir imtahan balına bax");
+            Console.WriteLine("4. Tələbənin bütün imtahanlarını göstər");
+            Console.WriteLine("5. Tələbənin imtahan ortalamasını göstər");
+            Console.WriteLine("6. Tələbədən imtahan sil");
+            Console.WriteLine("0. Proqramı bitir");
+
+            chose:
+                Console.WriteLine("Seciminizi edin:");
+                string option = Console.ReadLine();
+
+                switch(option)
+                {
+                    case "1":
+                        Console.WriteLine("");                      
+                        goto menu;
+                    case "2":
+                        Console.WriteLine();
+                        goto menu;
+                    case "3":
+                        Console.WriteLine();
+                        goto menu;
+                    case "4":
+                        Console.WriteLine();
+                        goto menu;
+                    case "5":
+                        Console.WriteLine();
+                        goto menu;
+                    case "6":
+                        Console.WriteLine();
+                        goto menu;
+                    case "0":
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Seciminiz yalnisdir");
+                        goto chose;
+                }
+
+            } while (true);
+
+
+        }
+    }
+}
---
+++ ConsoleApp13/ConsoleApp13/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+    class Student
+    {      
+        public int No { get; set; }
+        public string FullName { get; set; }
+
+        Dictionary<string, int> Exams = new Dictionary<string, int>();
+
+        public void AddExam(string examName, int point)
+        {
+            if(!string.IsNullOrWhiteSpace(examName))
+            Exams.Add(examName, point);
+        }
+
+        public int GetExamResult(string examName, int point)
+        {
+            if(!string.IsNullOrWhiteSpace(examName))
+            {
+                return Exams[examName];
+            }
+            return -1;
+        }
+
+        public int GetExamAvg()
+        {
+            int avarage = 0;
+            foreach (var value in Exams.Values)
+            {
+                avarage+=
+            }
+            return avarage;
+
+            }
+        }
+    }
+
