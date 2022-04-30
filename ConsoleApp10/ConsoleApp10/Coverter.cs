79b4474ffbbb670586b5cd982407ab975fd70a03
zeynallisahile <99805984+zeynallisahile@users.noreply.github.com>
2022-04-30 02:30:25 -0700

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Coverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+     class Coverter
+    {
+        public  double Exchange(Currency e,double azn)
+            {
+            if (e is Currency)
+            {
+                switch (e)
+                {
+                    case Currency.Usd:
+                        return azn * 0.59;
+                    case Currency.Eur:
+                        return azn * 0.53;
+                    case Currency.TRY:
+                        return azn * 8.73;
+                    case Currency.Rub:
+                        return azn * 52.21;
+                    default:
+                        return -1;                      
+                }
+            }
+            return -1;
+        }
+    }
+ }
+
---
+++ ConsoleApp10/ConsoleApp10/Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    enum  Currency
+    {
+        Usd,
+        Eur,
+        TRY,
+        Rub
+    }
+}
