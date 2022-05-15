3d87ceedcf8bd1c7c6d6e72b7fa9a6d3811e4b0e
zeynallisahile <99805984+zeynallisahile@users.noreply.github.com>
2022-05-15 12:26:57 +0400

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+     class Group
+    {
+        public string Name { get; set; }
+
+        List<Student> Students = new List<Student>();
+    }
+}
